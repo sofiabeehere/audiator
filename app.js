b19cc456ade02191011d8a19c9867ffd2142ab46
Sofia Bautista <sofiab@evidentpoint.com>
2020-04-05 01:40:32 -0700

    Implemented YouTube video ID and title retrieval to pass to Genius API wrapper to obtain song lyrics.

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require("path");
 const dotenv = require('dotenv').config();
+
 const stream = require('youtube-audio-stream');
 const session = require('express-session');
+const fetchVideoInfo = require('youtube-info');
+
+const genius = require("genius-lyrics");
+const Genius = new genius.Client(process.env.GENIUS_CLIENT_ACCESS_TOKEN);
+
 const app = express(); 
 
 app.use(express.urlencoded({ extended: false }));
@@ -16,10 +22,30 @@ app.get('/', function (req, res) {
 });
 
 app.post('/recognize', function (req, res) {
+
+    const getYouTubeID = (youtubeURL) => {
+        return youtubeURL.match(/(?:youtube.[a-z]+\/.*[\?\&]*v[/|=]|youtu.be\/)([0-9a-zA-Z-_]+)/)[1];
+    }
+
     req.session.youtubeURL = req.body.youtubeURL;
     req.session.save();
 
+    fetchVideoInfo(getYouTubeID(req.session.youtubeURL)).then(async function (videoInfo) {
+        const cleanTitle = videoInfo.title.replace(/ *\([^)]*\) */g, "");
+
+        console.log(cleanTitle);
+
+        const search = await Genius.findTrack(cleanTitle);
+        const url = await Genius.getUrl(search);
+        const lyricsJSON = await Genius.getLyrics(url);
+        const lyrics = lyricsJSON.lyrics;
+
+        console.log(lyrics);
+    });
+
     console.log(req.session.youtubeURL);
+    console.log(getYouTubeID(req.session.youtubeURL));
+
     res.render("recognize.ejs");
 })
 
