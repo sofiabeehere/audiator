7b230f922cd07f65b706db232c37d50a43af1ede
Sofia Bautista <sofiab@evidentpoint.com>
2020-04-06 21:02:32 -0700

    Changed TTS library from MacOS default to Festival.

--- forced-alignment.py
+++ forced-alignment.py
@@ -1,40 +1,16 @@
+from aeneas.tools.execute_task import ExecuteTaskCLI
 import sys
 args = sys.argv[1:]
-
-from aeneas.exacttiming import TimeValue
-from aeneas.executetask import ExecuteTask
-from aeneas.language import Language
-from aeneas.syncmap import SyncMapFormat
-from aeneas.task import Task
-from aeneas.task import TaskConfiguration
-from aeneas.textfile import TextFileFormat
-from aeneas.adjustboundaryalgorithm import AdjustBoundaryAlgorithm
-from aeneas.runtimeconfiguration import RuntimeConfiguration
-import aeneas.globalconstants as gc
-from aeneas.synthesizer import Synthesizer
-
-# create Task object
-config = TaskConfiguration()
-config[gc.PPN_TASK_LANGUAGE] = Language.ENG
-config[gc.PPN_TASK_IS_TEXT_FILE_FORMAT] = TextFileFormat.PLAIN
-config[gc.PPN_TASK_OS_FILE_FORMAT] = SyncMapFormat.VTT
-config[gc.PPN_TASK_ADJUST_BOUNDARY_NONSPEECH_MIN] = 0.001
-config[gc.PPN_TASK_ADJUST_BOUNDARY_ALGORITHM] = AdjustBoundaryAlgorithm.AUTO
-rconf = RuntimeConfiguration()
-rconf[RuntimeConfiguration.TTS] = Synthesizer.MACOS
-rconf.set_granularity(3)
-rconf[RuntimeConfiguration.MFCC_MASK_NONSPEECH] = False
-rconf[RuntimeConfiguration.MFCC_MASK_NONSPEECH_L1] = False
-rconf[RuntimeConfiguration.MFCC_MASK_NONSPEECH_L2] = False
-rconf[RuntimeConfiguration.MFCC_MASK_NONSPEECH_L3] = False
-task = Task()
-task.configuration = config
-task.audio_file_path_absolute = args[0]
-task.text_file_path_absolute = args[1]
-task.sync_map_file_path_absolute = args[2]
-
-# process Task
-ExecuteTask(task, rconf=rconf).execute()
-
-# print produced sync map
-task.output_sync_map_file()
+ExecuteTaskCLI(use_sys=False).run(arguments=[
+    None,  # dummy program name argument
+    args[0],
+    args[1],
+    ("tts=festival|"
+     "task_language=eng|"
+     "os_task_file_format=vtt|"
+     "is_text_type=plain|"
+     "task_adjust_boundary_nonspeech_min=0.0010|"
+     "task_adjust_boundary_algorithm=auto|"
+     "level=3"),
+    args[2]
+])
